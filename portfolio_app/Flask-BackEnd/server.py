9a59e00e3e2eb44c5f0770aee9b6f08d42da6482
vladimirvuletic002 <vladimirvuletic12@gmail.com>
2025-07-08 02:43:10 +0200

    frontend deploy

--- portfolio_app/Flask-BackEnd/server.py
+++ portfolio_app/Flask-BackEnd/server.py
@@ -35,7 +35,7 @@ bcrypt = Bcrypt(app)
 # Omogućavanje CORS-a za sve rute
 #CORS(app, supports_credentials=True, resources={r"/*": {"origins": ["http://localhost:3000"]}})
 #CORS(app, supports_credentials=True, expose_headers=["Authorization"])
-CORS(app, supports_credentials=True, origins=["http://localhost:3000","https://drs-jv2m-frontend.onrender.com", "https://drs-jv2m.onrender.com"], expose_headers=["Authorization"])
+CORS(app, supports_credentials=True, origins=["http://localhost:3000","drs-omega.vercel.app", "https://drs-jv2m.onrender.com", "drs-fmnatc3vs-vladimirs-projects-6e53e3c9.vercel.app"], expose_headers=["Authorization"])
 #CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 
